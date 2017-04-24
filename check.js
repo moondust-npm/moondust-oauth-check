c606915e769cb9da23fa6ccb635a4d3ad63207ff
tc949 <tc949561391@hotmai.com>
2017-04-24 18:01:22 +0800

    init

---
+++ check.js
@@ -0,0 +1,6 @@
+/**
+ * Created by tc949 on 2017/4/24.
+ */
+module.exports=(access_token)=>{
+
+};
--- index.js
+++ index.js
@@ -6,28 +6,27 @@
  * @param scopes 访问该路由所需要的所有权限
  * @returns {function(*, *)}
  */
-let authorization = (scopes) => {
-    return async (ctx, next) => {
-        let access_token = ctx.accept.headers['authorization'] ||
-            ctx.query.authorization ||
-            ctx.request.body.authorization;
+const precheck = require('./check');
 
-        if (util.isNullOrUndefined(access_token)) {
-            throw new BusinessError(500, 'access token is none');
+
+let authorization = (ops) => {
+    this.url = ops.url;
+    this.precheck = ops.precheck || precheck;
+
+    this.check = () => {
+        return async (ctx, next) => {
+            let access_token = ctx.accept.headers['authorization'] ||
+                ctx.query.authorization ||
+                ctx.request.body.authorization;
+            let res = await this.precheck(access_token);
+            if (!res) throw new Error('access_token cant check');
+            let i = 'asd';
+            ctx.principle = i;
+            ctx.user_id = i.user_id;
+            next();
         }
-        let i = await token_repository.find_by_access_token(access_token);
-        if (util.isNullOrUndefined(i) || util.isNullOrUndefined(i.user_id)) {
-            throw new BusinessError(500, 'access_token is timeout or useless');
-        }
-        if (i.scope !== 'all') {
-            let allInclude = await commonUtil.include(scopes, i.scope, ',');
-            if (!allInclude) {
-                throw new BusinessError(500, `${i.scope} can not match ${scopes}`);
-            }
-        }
-        ctx.principle = i;
-        ctx.user_id = i.user_id;
-        next()
     }
 };
+
+
 module.exports = authorization;
